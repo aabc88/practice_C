bd0dd4ad5c4642dd9d696d1481b1e4c19c68f8c0
aabc88 <aabc88@nate.com>
2020-06-25 03:36:13 -0700

    ,

---
+++ 5/5-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("char : %d, short : %d, int : %d, long : %d, long long : %d\n",
+
+		sizeof(char),
+		sizeof(short),
+		sizeof(int),
+		sizeof(long),
+		sizeof(long long)
+	      );
+	
+	return 0;
+}
+
