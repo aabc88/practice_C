1ed380ef4054e21661064d65a46bf6858015bb96
aabc88 <aabc88@nate.com>
2020-07-07 01:09:32 -0700

    simple coding

---
+++ UNIT10/10-1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", 10);
+	printf("0%o\n", 017);
+	printf("0x%x\n", 0x1f);
+
+	return 0;
+}
---
+++ UNIT9/9-3-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	char linefeed = '\n';
+
+	printf("%c, 0x%x\n", linefeed, linefeed);
+	return 0;
+}
